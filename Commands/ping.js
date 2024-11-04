d29a4d7e82f90842db10c7d954452b761544b2d2
Nathan <nathan.agu.2103@gmail.com>
2024-11-04 18:24:30 +0100

    feature: loader des SlashCommands + update commande ping

--- Commands/ping.js
+++ Commands/ping.js
@@ -1,7 +1,12 @@
 const Discord = require("discord.js")
 
 module.exports = {
+
     name: "ping",
+    description: "",
+    permission: "Aucune",
+    dm: true,
+
     async run(bot, message) {
         await message.reply(`Ping : \`${bot.ws.ping}\``)
     }
