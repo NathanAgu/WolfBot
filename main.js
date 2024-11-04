b98109ca0278c042662d271cdd7120522db365af
Nathan <nathan.agu.2103@gmail.com>
2024-11-04 21:06:51 +0100

    update: Loaders -> Handler

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js")
 const intents = new Discord.IntentsBitField(3276799)
 const bot = new Discord.Client({intents})
-const loadCommands = require("./Loaders/loadCommands")
-const loadEvents = require("./Loaders/loadEvents")
+const loadCommands = require("./Handler/loadCommands")
+const loadEvents = require("./Handler/loadEvents")
 const config = require("./config")
 
 bot.commands = new Discord.Collection()
