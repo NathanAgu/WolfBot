2ab68da55156d3facbca708cabd75fd370720566
Nathan <nathan.agu.2103@gmail.com>
2024-11-04 16:04:21 +0100

    Feature: Création et initialisation du bot

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("test")
+const Discord = require("discord.js")
+const bot = new Discord.Client({intents : 3276799})
+const config = require ("./config")
+
+bot.login(config.token)
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.tag} est en ligne !`)
+})
