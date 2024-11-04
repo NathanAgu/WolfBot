69c51f8ba3d89cf880180bcdc28eac77ddf9f80c
Nathan <nathan.agu.2103@gmail.com>
2024-11-04 16:31:55 +0100

    update: loader des commandes

--- Loaders/loadCommands.js
+++ Loaders/loadCommands.js
@@ -1,8 +1,8 @@
 const fs = require("fs")
 
 module.exports = async bot => {
-    fs.readdirSync("./Commands").filter(f => f.endsWith(".js")).then(async file => {
-        let command = require(`../Commands/${file}.js`)
+    fs.readdirSync("./Commands").filter(f => f.endsWith(".js")).forEach(async file => {
+        let command = require(`../Commands/${file}`)
         if (!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file} n'a pas de nom !`)
         bot.commands.set(command.name, command)
         console.log(`Commande ${file} chargée avec succès !`)
