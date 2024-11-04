2baeaab6cc2c790aed95b3bd14e8de874b8b8b19
Nathan <nathan.agu.2103@gmail.com>
2024-11-04 21:05:16 +0100

    code clean

--- Loaders/loadEvents.js
+++ Loaders/loadEvents.js
@@ -4,7 +4,7 @@ module.exports = async bot => {
     fs.readdirSync("./Events").filter(f => f.endsWith(".js")).forEach(async file => {
 
         let event = require(`../Events/${file}`)
-        
+
         bot.on(file.split(".js").join(""), event.bind(null, bot))
         console.log(`[+] ${file} : Succès !`)
     })
